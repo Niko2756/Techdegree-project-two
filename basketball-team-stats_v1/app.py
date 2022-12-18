31f542007528e6bc974f43885f2d769f248d378b
Niko Lim <theniko1@gmail.com>
2022-12-17 22:03:26 -0800

    done
    finished

--- basketball-team-stats_v1/app.py
+++ basketball-team-stats_v1/app.py
@@ -5,11 +5,23 @@ def main():
 	data = constants.PLAYERS.copy()
 	theTeams = constants.TEAMS.copy()
 	dataThatHasBeenCleaned = fuctions.clean_the_data(data)
-	#print(dataThatHasBeenCleaned)
 	theTeamsBalanced, num_players_team = fuctions.balance_the_teams(theTeams, dataThatHasBeenCleaned)
 	theTeamsBalancedButJustNames = fuctions.info_stripper_leave_just_name(theTeamsBalanced, num_players_team)
-	print()
-
+	print("basketball team stat tool")
+	while True:
+		print("\n\n-----MENU-----\n\n")
+		print("Here are your choices:\n")
+		userInput = fuctions.your_first_choice()
+		if userInput == 1:
+			for x, team in enumerate(theTeams):
+				print(f'{x+1}) {team}')
+			secondUserInput = fuctions.your_second_choice(theTeams)
+			print(f"\nTeam: {theTeams[secondUserInput-1]}\n")
+			print(', '.join(map(str, theTeamsBalancedButJustNames[secondUserInput-1])))
+			print(f"Total players: {len(theTeamsBalancedButJustNames[secondUserInput-1])} \n")
+		else:
+			print("Thank you so much for using this thing.\nThis app has been coded by Niko.")
+			break
 
 
 
--- basketball-team-stats_v1/fuctions.py
+++ basketball-team-stats_v1/fuctions.py
@@ -26,8 +26,6 @@ def balance_the_teams(theTeams, dataThatHasBeenCleaned):
 
     return theTeamsBalanced, num_players_team
 
-#print(theTeamsBalanced[0][5].get("name"))
-
 
 def info_stripper_leave_just_name(theTeamsBalanced, num_players_team):
     theTeamsBalancedButJustNames = []
@@ -41,11 +39,40 @@ def info_stripper_leave_just_name(theTeamsBalanced, num_players_team):
     return theTeamsBalancedButJustNames
 
 
+def your_first_choice():
+    while True:
+        try:
+            userInput = int(input("Input 1 to display team stats\nInput 2 to quit program\n(or press enter to go straight through): ")or 1)
+        except ValueError:
+            print("Hey I don't think that's even a number. \nGo ahead and enter the number of userInput again,\nbut make sure it's like a actual number this time:\n\n")
+            continue
+        if userInput not in (1,2):
+            print("\nthat is not a viable option\n")
+            continue
+        else:
+            return userInput
+
+
+def your_second_choice(theTeams):
+    while True:
+        try:
+            userInput = int(input("Please choose from the above options which team you would like to see: "))
+        except ValueError:
+            print("Hey I don't think that's even a number. \nGo ahead and enter the number of userInput again,\nbut make sure it's like a actual number this time:\n\n")
+            continue
+        if userInput not in range(1, (len(theTeams)+1)):
+            print("\nthat is not a viable option\n")
+            continue
+        else:
+            return userInput
+
+
 def main():
     clean_the_data()
     balance_the_teams()
     info_stripper_leave_just_name()
-
+    your_first_choice()
+    your_second_choice()
 
 if __name__ == '__main__':
     main()
